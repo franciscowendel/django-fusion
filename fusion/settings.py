f99cf34e37fa62cff36749ff6371115efc04a6c9
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-08 20:40:29 -0300

    Usando o 'python-decouple' pra isolar as variáveis de ambiente no 'settings.py'

--- fusion/settings.py
+++ fusion/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'fusion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
