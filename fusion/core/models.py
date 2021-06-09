5b74c9a778acd2b7946bc0a3a8d17e55b929e675
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-09 18:53:12 -0300

    models criados close #10

--- fusion/core/models.py
+++ fusion/core/models.py
@@ -55,3 +55,36 @@ class Employee(Base):
     name = models.CharField(max_length=120)
     position = models.ForeignKey(Position, related_name='employess', on_delete=models.CASCADE)
     bio = models.TextField(default='', blank=True)
+    image = StdImageField(upload_to=shuffle_filename, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField(max_length=60, default='#')
+    twitter = models.CharField(max_length=60, default='#')
+    instagram = models.CharField(max_length=60, default='#')
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+
+class Feature(Base):
+    ICONES_FEATURES = (
+        ('lni-rocket', 'Foguete'),
+        ('lni-laptop-phone', 'API'),
+        ('lni-cog', 'Engrenagem'),
+        ('lni-leaf', 'Folha'),
+        ('lni-layers', 'Camadas'),
+        ('lni-leaf', 'Folha'),
+    )
+    feature = models.CharField(max_length=120)
+    description = models.TextField(default='', blank=True)
+    icon = models.CharField(max_length=60, choices=ICONES_FEATURES)
+
+    class Meta:
+        verbose_name = 'Feature'
+        verbose_name_plural = 'Features'
+
+    def __str__(self):
+        return self.feature
