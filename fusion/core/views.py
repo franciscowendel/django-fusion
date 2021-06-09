867b89666e8eeee243c98ebafd88fc96a69a9ab0
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-09 20:21:02 -0300

    Contexto criado close #15

--- fusion/core/views.py
+++ fusion/core/views.py
@@ -1,5 +1,22 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from .models import (
+    Service,
+    Employee,
+    Feature,
+
+)
+from .forms import ContactForm
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.order_by('?').all()  # noqa
+        context['employess'] = Employee.objects.order_by('?').all()  # noqa
+        context['features'] = Feature.objects.order_by('?').all()  # noqa
+        return context
