5af4185ea764ea5655b5dd2ac9e93033f663a68d
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-12 20:50:54 -0300

    'serializers.py' criado no app 'core' e models registrados neste close #28

---
+++ fusion/core/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from .models import (
+    Service,
+    Position,
+    Employee,
+    Feature,
+)
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Service
+        fields = (
+            'id',
+            'service',
+            'description',
+            'created',
+            'updated',
+            'is_active',
+        )
+
+
+class PositionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = Position
+        fields = (
+            'id',
+            'position',
+            'created',
+            'updated',
+            'is_active',
+        )
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Employee
+        fields = (
+            'id',
+            'name',
+            'position',
+            'bio',
+            'facebook',
+            'twitter',
+            'instagram',
+            'created',
+            'updated',
+            'is_active',
+        )
+
+
+class FeatureSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Feature
+        fields = (
+            'id',
+            'feature',
+            'description',
+            'created',
+            'updated',
+            'is_active',
+        )
