5a42aa7e5c08f157da9ac47c45c87b50b456bc98
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-12 20:56:52 -0300

    path para API versão 1 criado; Adicionado path de login do rest_framework close #29

--- fusion/urls.py
+++ fusion/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('api/v1/', include('fusion.core.urls')),
+    path('auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', include('fusion.core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
