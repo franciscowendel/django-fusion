06c5b7132b943b714e7157b646e92ae60de5ab6b
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-12 21:07:34 -0300

    'rest_framework_throttling' corrigido 'rest_framework.throttling'

--- fusion/settings.py
+++ fusion/settings.py
@@ -167,15 +167,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReaOnly',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
 
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework_throttling.AnonRateThrottle',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle',
-    ),
+    ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '10/minute',
         'user': '20/minute',
