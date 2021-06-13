09d53b1f480355c0e6091f173e8f53ccaed189b1
franciscowendel <iamfranciscowendel@gmail.com>
2021-06-12 21:23:24 -0300

    path para o CRUD das views criado close #31

--- fusion/core/urls.py
+++ fusion/core/urls.py
@@ -1,6 +1,33 @@
 from django.urls import path
-from .views import IndexView
+from .views import (
+    IndexView,
+    ServicesAPIView,
+    ServiceAPIView,
+    PositionsAPIView,
+    PositionAPIView,
+    EmployeesAPIView,
+    EmployeeAPIView,
+    FeaturesAPIView,
+    FeatureAPIView,
+)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+
+    # services
+    path('services/', ServicesAPIView.as_view(), name='services'),
+    path('services/<int:pk>/', ServiceAPIView.as_view(), name='service'),
+
+    # positions
+    path('positions/', PositionsAPIView.as_view(), name='positions'),
+    path('positions/<int:pk>/', PositionAPIView.as_view(), name='position'),
+
+    # employees
+    path('employees/', EmployeesAPIView.as_view(), name='employees'),
+    path('employees/<int:pk>/', EmployeeAPIView.as_view(), name='employee'),
+
+    # features
+    path('features/', FeaturesAPIView.as_view(), name='features'),
+    path('features/<int:pk>/', FeatureAPIView.as_view(), name='feature'),
+
 ]
