61cab05f111c2d4be6b2a2ccaddb26cbc1b7f8c9
Francisco <cisco.wendel@gmail.com>
2024-01-13 17:28:32 -0300

    Update urls.py

--- fusion/urls.py
+++ fusion/urls.py
@@ -14,16 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import (
+    path, 
+    include
+)
 from django.conf.urls.static import static
 from django.conf import settings
 
 from fusion.core.urls import router
 
 urlpatterns = [
-    path('api/v1/', include('fusion.core.urls')),
-    path('api/v2/', include(router.urls)),
-    path('auth/', include('rest_framework.urls')),
+    # DJANGO ADMIN
     path('admin/', admin.site.urls),
+
+    # DJANGO REST FRAMEWORK ADMIN
+    path('auth/', include('rest_framework.urls')),
+
+    # CORE URLS
     path('', include('fusion.core.urls')),
+
+    # API (1) Generics
+    path('api/v1/', include('fusion.core.urls')),
+
+    # API (2) Viewsets
+    path('api/v2/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
