788647d10f5d3b471935a705031f50f4fc935957
Francisco <cisco.wendel@gmail.com>
2024-01-13 17:42:42 -0300

    Update models.py

--- fusion/core/models.py
+++ fusion/core/models.py
@@ -19,41 +19,42 @@ class Base(models.Model):
 
 
 class Service(Base):
-    ICONE_SERVICES = (
-        ('lni-cog', 'Engrenagem'),
-        ('lni-stats-up', 'Estatísticas'),
-        ('lni-users', 'Usuários'),
-        ('lni-layers', 'Camadas'),
+    SERVICES_ICONS = (
+        ('lni-cog', 'Cog'),
+        ('lni-stats-up', 'Stats'),
+        ('lni-users', 'Users'),
+        ('lni-layers', 'Layers'),
         ('lni-mobile', 'Mobile'),
-        ('lni-rocket', 'Foguete')
+        ('lni-rocket', 'Rocket')
     )
     service = models.CharField(max_length=120)
     description = models.TextField(default='', blank=True)
-    icon = models.CharField(choices=ICONE_SERVICES, max_length=50)
+    icon = models.CharField(choices=SERVICES_ICONS, max_length=50)
 
     class Meta:
         verbose_name = 'Service'
         verbose_name_plural = 'Services'
+        ordering = ['id', ]
 
     def __str__(self):
         return self.service
 
 
-class Position(Base):
-    position = models.CharField(max_length=130)
+class Role(Base):
+    role = models.CharField(max_length=130)
 
     class Meta:
-        verbose_name = 'Position'
-        verbose_name_plural = 'Positions'
+        verbose_name = 'Role'
+        verbose_name_plural = 'Roles'
         ordering = ['id']
 
     def __str__(self):
-        return self.position
+        return self.role
 
 
 class Employee(Base):
     name = models.CharField(max_length=120)
-    position = models.ForeignKey(Position, related_name='employees', on_delete=models.CASCADE)
+    position = models.ForeignKey(Role, related_name='employees', on_delete=models.CASCADE)
     bio = models.TextField(default='', blank=True)
     image = StdImageField(upload_to=shuffle_filename, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
     facebook = models.CharField(max_length=60, default='#')
@@ -70,7 +71,7 @@ class Employee(Base):
 
 
 class Feature(Base):
-    ICONES_FEATURES = (
+    FEATURES_ICONS = (
         ('lni-rocket', 'Foguete'),
         ('lni-laptop-phone', 'API'),
         ('lni-cog', 'Engrenagem'),
@@ -80,11 +81,12 @@ class Feature(Base):
     )
     feature = models.CharField(max_length=120)
     description = models.TextField(default='', blank=True)
-    icon = models.CharField(max_length=60, choices=ICONES_FEATURES)
+    icon = models.CharField(max_length=60, choices=FEATURES_ICONS)
 
     class Meta:
         verbose_name = 'Feature'
         verbose_name_plural = 'Features'
+        ordering = ['id', ]
 
     def __str__(self):
         return self.feature
